63582b56e9d20e6c493b88534459f1e39d9b2ac1
banosdavid <banosdavid05@gmail.com>
2025-02-12 19:58:18 +0100

    add psycopg

--- src/data/db.py
+++ src/data/db.py
@@ -14,7 +14,7 @@ engine =create_engine(os.getenv("db_url", DATABASE_URL, echo=True))
 def get_session():
     with Session(engine) as session:
         yield session
-
+        
 def init_db():
     SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
