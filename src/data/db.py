8c12fa90c81a46ce3e61977ae6ee0ae92b1aec00
banosdavid <banosdavid05@gmail.com>
2025-02-12 20:03:18 +0100

    arreglo de url externa

--- src/data/db.py
+++ src/data/db.py
@@ -9,12 +9,12 @@ db_port: str = 3306
 db_name: str = "vehiculodb"
 
 DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_server}:{db_port}/{db_name}"
-engine =create_engine(os.getenv("db_url", DATABASE_URL, echo=True))
+engine =create_engine(os.getenv("db_url", DATABASE_URL))
 
 def get_session():
     with Session(engine) as session:
         yield session
-        
+
 def init_db():
     SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
